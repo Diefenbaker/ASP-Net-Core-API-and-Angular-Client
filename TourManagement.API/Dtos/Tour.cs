fb5599e4c41501452d03fea9a0104c4295734e9e
Brian Phillips <bphillipswex@outlook.com>
2018-10-01 17:34:08 +0100

    Adding project files

---
+++ TourManagement.API/Dtos/Tour.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TourManagement.API.Dtos
+{
+    public class Tour: TourAbstractBase    {
+        public Guid TourId { get; set; }
+        public string Band { get; set; }
+    }
+}
